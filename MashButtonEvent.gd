948ad06c960ca9906784f7331d7ba107610cbaa0
JTNguyen77 <ragnornexus77@gmail.com>
2026-09-17 23:56:58 -0400

    WIP Mashbutton

--- MashButtonEvent.gd
+++ MashButtonEvent.gd
@@ -1,27 +1,48 @@
 extends Node2D
 
 var jump_count: int = 0
-var required_jumps: int = 20
 var event_active: bool = false
+var required_jumps: int = 20
+
+var player = null
+var saved_position: Vector2
+
+onready var mash_label: Label = $MashLabel
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _ready() -> void:
+	mash_label.visible = false
 
 
 func _on_Trigger_body_entered(body):
-	if body.name == "Player":
+	if body.name == "Player" and not event_active:
+		player = body
+		saved_position = body.position
+
+		jump_count = 0
 		event_active = true
-		print ("HE ENTERED THE THINGGGGGGG")
+
+		body.event_mode = true
+
+		mash_label.text = "MASH SPACE! " + str(jump_count) + "/20"
+		mash_label.visible = true
+
 
 func _process(delta: float) -> void:
 	if event_active:
 		if Input.is_action_just_pressed("jump"):
 			jump_count += 1
-			print("Jump presses: ", jump_count);
+
+			mash_label.text = "MASH SPACE! " + str(jump_count) + "/20"
+
+			print("Jump presses: ", jump_count)
+
+			if jump_count >= required_jumps:
+				event_active = false
+
+				player.position = saved_position
+				player.event_mode = false
+
+				mash_label.visible = false
+
+				print("Mash event complete!")
--- Player.gd
+++ Player.gd
@@ -5,22 +5,30 @@ export var jump_force: float = 220.0
 export var gravity: float = 700.0
 
 var velocity: Vector2 = Vector2.ZERO
+var event_mode: bool = false
 
 onready var sprite: AnimatedSprite = $AnimatedSprite
 
 
 func _physics_process(delta: float) -> void:
 	var direction: float = 0.0
-	if Input.is_action_pressed("move_right"):
-		direction += 1.0
-	if Input.is_action_pressed("move_left"):
-		direction -= 1.0
 
-	velocity.x = direction * speed
-	velocity.y += gravity * delta
+	if not event_mode:
+		if Input.is_action_pressed("move_right"):
+			direction += 1.0
 
-	if is_on_floor() and Input.is_action_just_pressed("jump"):
-		velocity.y = -jump_force
+		if Input.is_action_pressed("move_left"):
+			direction -= 1.0
+
+		velocity.x = direction * speed
+		velocity.y += gravity * delta
+
+		if is_on_floor() and Input.is_action_just_pressed("jump"):
+			velocity.y = -jump_force
+
+	else:
+		velocity.x = 0.0
+		velocity.y = 0.0
 
 	velocity = move_and_slide(velocity, Vector2.UP)
 
@@ -32,6 +40,7 @@ func _update_animation(direction: float) -> void:
 		sprite.flip_h = direction < 0.0
 
 	var next_anim: String = "idle"
+
 	if direction != 0.0:
 		next_anim = "walk"
 
