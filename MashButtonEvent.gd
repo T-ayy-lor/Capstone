9c7b57d3387e43d014fb2f9864fde4e9cac5a8c0
JTNguyen77 <nguyentjason1705@gmail.com>
2026-09-18 16:04:58 -0400

    Added a teleporter to a different area for mashbutton

--- MashButtonEvent.gd
+++ MashButtonEvent.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var jump_count: int = 0
 var event_active: bool = false
+var trigger_disabled: bool = false
 var required_jumps: int = 20
 
 var player = null
@@ -15,10 +16,12 @@ func _ready() -> void:
 
 
 func _on_Trigger_body_entered(body):
-	if body.name == "Player" and not event_active:
+	if body.name == "Player" and not event_active and not trigger_disabled:
 		player = body
 		saved_position = body.position
-
+		
+		body.position = dimension_spawn.position
+		
 		jump_count = 0
 		event_active = true
 
@@ -39,10 +42,23 @@ func _process(delta: float) -> void:
 
 			if jump_count >= required_jumps:
 				event_active = false
+				trigger_disabled = true
 
-				player.position = get_node("../DimensionSpawn").position
+				player.position = saved_position
 				player.event_mode = false
 
 				mash_label.visible = false
-
+				
+				get_parent().score += 20
+				get_parent().score_label.text = str(get_parent().score)
 				print("Mash event complete!")
+
+	if trigger_disabled and player != null:
+		if player.position.distance_to(saved_position) > 100:
+			trigger_disabled = false
+			print("Mash trigger re-enabled!")
+
+func _on_DimensionArea_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	if body.name == "Player":
+		player.position = saved_position
+		player.event_mode = false
